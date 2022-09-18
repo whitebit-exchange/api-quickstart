6f191390ff462bb583301467037b5b1131f510e2
Danipulok <danipulok@gmail.com>
2022-09-18 05:23:45 +0300

    Fix `nonce` generation in python

--- src/python/auth.py
+++ src/python/auth.py
@@ -1,34 +1,34 @@
-import json
 import base64
-import requests
-import time
-import hmac
 import hashlib
+import hmac
+import json
+import time
+
+import requests
 
 
-api_key = '' #put here your public key
-secret_key = '' #put here your secret key
-request = '/api/v4/trade-account/balance' #put here request path. For obtaining trading balance use: /api/v4/trade-account/balance
-baseUrl = 'https://whitebit.com' #domain without last slash. Do not use https://whitebit.com/
-#If the nonce is similar to or lower than the previous request number, you will receive the 'too many requests' error message
-nonce = str(int(time.time())) #nonce is a number that is always higher than the previous request number
-
+api_key = ''  # put here your public key
+secret_key = ''  # put here your secret key
+request = '/api/v4/trade-account/balance'  # put here request path. For obtaining trading balance use: /api/v4/trade-account/balance
+baseUrl = 'https://whitebit.com'  # domain without last slash. Do not use https://whitebit.com/
+# If the nonce is similar to or lower than the previous request number, you will receive the 'too many requests' error message
+nonce = time.time_ns() // 1_000_000  # nonce is a number (preferrably epoch time in milliseconds) that is always higher than the previous request number
 
 data = {
-  'ticker': 'BTC', #for example for obtaining trading balance for BTC currency
-  'request': request,
-  'nonce': nonce,
-    'nonceWindow': True # the api will validate that your nonce enter the range of current time +/- 5 seconds
+    'ticker': 'BTC',  # for example for obtaining trading balance for BTC currency
+    'request': request,
+    'nonce': nonce,
+    'nonceWindow': True  # the api will validate that your nonce enter the range of current time +/- 5 seconds
 }
 
-#preparing request URL
+# preparing request URL
 completeUrl = baseUrl + request
 
-data_json = json.dumps(data, separators=(',', ':')) #use separators param for deleting spaces
+data_json = json.dumps(data, separators=(',', ':'))  # use separators param for deleting spaces
 payload = base64.b64encode(data_json.encode('ascii'))
 signature = hmac.new(secret_key.encode('ascii'), payload, hashlib.sha512).hexdigest()
 
-#preparing headers
+# preparing headers
 headers = {
     'Content-type': 'application/json',
     'X-TXC-APIKEY': api_key,
@@ -36,8 +36,8 @@ headers = {
     'X-TXC-SIGNATURE': signature,
 }
 
-#sending request
+# sending request
 resp = requests.post(completeUrl, headers=headers, data=data_json)
 
-#receiving data
+# receiving data
 print(json.dumps(resp.json(), sort_keys=True, indent=4))
